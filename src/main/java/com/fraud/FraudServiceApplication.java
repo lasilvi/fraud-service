17fbc8ddb67c90199fd15a111b704815db75786b
Silvania Ramos Paternina <silvania.ramos@udea.edu.co>
2026-03-24 15:04:27 -0500

    chore(bootstrap): add Maven, Docker, and app entrypoint skeleton

---
+++ src/main/java/com/fraud/FraudServiceApplication.java
@@ -0,0 +1,8 @@
+package com.fraud;
+
+public class FraudServiceApplication {
+
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException("Bootstrap not implemented yet");
+    }
+}
